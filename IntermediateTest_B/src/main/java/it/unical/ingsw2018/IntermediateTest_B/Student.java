cb8392a983bce46bfd63fcc5a3703cb9ca3b560e
RossellaC <rossella.calabretta89@gmail.com>
2018-11-22 17:06:01 +0100

    Prova

--- IntermediateTest_B/src/main/java/it/unical/ingsw2018/IntermediateTest_B/Student.java
+++ IntermediateTest_B/src/main/java/it/unical/ingsw2018/IntermediateTest_B/Student.java
@@ -21,12 +21,33 @@ public class Student {
 	public List<Exam> getCarrier() {
 		return carrier;
 	}
+	private static Exam c;
+  
+	public  void registerExam(Exam exam) {
+	   carrier.add(exam);
+	   if(exam==c) {
+		   throw new IllegalArgumentException();
+   }
+	}
+	   public int getCreditSum() {
+		   int sum=0;
+		   for(int i=0;i<carrier.size();i++) {
+			   sum=sum+((Exam) carrier).getCfu();	   
+		   }
+		   return sum;
+	   }
 
-	
-	
-	
-	
-	
+	   public double getWeightedSum() {
+		   int m=0;
+		   int media=0;
+		   for(int i=0;i<carrier.size();i++) {
+			  m=((Exam) carrier).getGrade() *((Exam) carrier).getCfu();	
+			  media=media+m;
+			  m=0;
+		   }
+		   
+		   return media/getCreditSum();
+	   }
 	
 
 }
--- IntermediateTest_B/src/test/java/it/unical/ingsw2018/IntermediateTest_B/CarrierSimpleTest.java
+++ IntermediateTest_B/src/test/java/it/unical/ingsw2018/IntermediateTest_B/CarrierSimpleTest.java
@@ -1,6 +1,20 @@
 package it.unical.ingsw2018.IntermediateTest_B;
 
+import org.junit.Test;
+
 public class CarrierSimpleTest {
 
+	//private static Exam c;
+	private static Student f;
+	@Test(expected = IllegalArgumentException.class)
+	
+	public void registerExamTest() {
+		Exam q= new Exam("mat", 0, 0);
+		
+	 f.registerExam(q);
+	 f.registerExam(q);
+	 
+		}
+		
 	
 }
