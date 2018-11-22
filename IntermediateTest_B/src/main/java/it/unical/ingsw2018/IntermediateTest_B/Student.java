d0770e08291e9b4960b08d8e33ccbcfe201022e4
Ettor <Ettor@160.97.140.236>
2018-11-22 17:04:59 +0100

    primo

--- IntermediateTest_B/src/main/java/it/unical/ingsw2018/IntermediateTest_B/Student.java
+++ IntermediateTest_B/src/main/java/it/unical/ingsw2018/IntermediateTest_B/Student.java
@@ -23,10 +23,53 @@ public class Student {
 	}
 
 	
+	public void registerExam(Exam exam) throws Exception
+	{
+		for (Exam currentExam : carrier) {
+			if (carrier.isEmpty())
+			{
+				carrier.add(exam); 
+				return ; 
+			}
+			if (currentExam.equals(exam))
+			{
+				throw new Exception() ;
+			}
+			this.carrier.add(exam); 
+		}
+	}
+	
+	public int getCreditSum() {
+		int sum = 0 ; 
+		for (Exam exam : carrier) {
+			sum += exam.getGrade() ; 
+		}
+		return sum  ;
+	}
+	
+	public double getWeightedSum() {
+		int value = 0 ; 
+		for (Exam exam : carrier) {
+			value += (exam.getCfu()*exam.getGrade()) ;
+		}
+		value /= 174  ;
+		value /=3 ; 
+		return value*= 11 ; 
+		
+	}
 	
 	
+	public static void main(String[] args) throws Exception {
+		Student s = new  Student("studente") ; 
+		Exam e1 = new  Exam("nome", 30, 2) ;
+		Exam e2 = new  Exam("nome", 30, 2) ;
+		
+		s.registerExam(e1);
+		
+		System.out.println(s.getCarrier().get (0));
+	}
 	
 	
-	
+
 
 }
