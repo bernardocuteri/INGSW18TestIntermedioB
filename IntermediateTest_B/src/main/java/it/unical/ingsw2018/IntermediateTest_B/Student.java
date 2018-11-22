7101ce23e9816bab88670c788759650359697d4d
Luca <Luca@DESKTOP-2KICHS1>
2018-11-22 17:03:54 +0100

    commit TestIntermedioB

--- IntermediateTest_B/src/main/java/it/unical/ingsw2018/IntermediateTest_B/Student.java
+++ IntermediateTest_B/src/main/java/it/unical/ingsw2018/IntermediateTest_B/Student.java
@@ -22,11 +22,39 @@ public class Student {
 		return carrier;
 	}
 
+	public void registerExam(Exam exam) {
+		boolean trovato = false;
+		for(int i = 0 ; i < carrier.size() ; i++) {
+			if(carrier.get(i).getName() == exam.getName()) {
+				trovato = true;
+				break;
+			}	
+		}
+		if(trovato)
+			throw new IllegalArgumentException("esame gia inserito: " + exam.getName());
+		else
+			carrier.add(exam);
+	}
 	
-	
-	
-	
-	
-	
+	public int getCreditSum() {
+		int totaleCfu = 0;
+		for(int i = 0 ; i < carrier.size() ; i++) {
+			totaleCfu += carrier.get(i).getCfu();
+		}
+		
+		return totaleCfu;
+	}
+
+	public double getWeightedSum() {
+		double media = 0;
+		int count = 0;
+		
+		for(int i = 0 ; i < carrier.size() ; i++) {
+			media += (carrier.get(i).getGrade()*carrier.get(i).getCfu());
+			count += carrier.get(i).getCfu();
+		}
+		
+		return media/count;
+	}
 
 }
--- IntermediateTest_B/src/test/java/it/unical/ingsw2018/IntermediateTest_B/CarrierAdvancedTest.java
+++ IntermediateTest_B/src/test/java/it/unical/ingsw2018/IntermediateTest_B/CarrierAdvancedTest.java
@@ -1,5 +1,30 @@
 package it.unical.ingsw2018.IntermediateTest_B;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class CarrierAdvancedTest {
  
+	private static Student student;
+	
+	@BeforeClass
+	public static void init() {
+		student = new Student("Giovanni");
+	}
+	
+	@Test
+	public void calcolaMedia() {
+		Exam exam1 = new Exam("PO", 22, 10);
+		Exam exam2 = new Exam("IG", 28, 5);
+		Exam exam3 = new Exam("SIW", 27, 5);
+		
+		student.registerExam(exam1);
+		student.registerExam(exam2);
+		student.registerExam(exam3);
+		
+		assertEquals(24.75, student.getWeightedSum(),0.1);
+	}
 }
